be237d979e38f55a37823615a7ce7e2796cdb005
Jeremias <jeremiastpereira@gmail.com>
2015-07-06 16:06:33 -0300

    SQL

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # fazer-feito
-Uma pequena aplicação de lista de tarefas em PHP e AngularJs.
+Uma pequena aplicação de lista de tarefas em PHP e AngularJs, feita com o intuito de estudar.
+
+As requisições ao banco são feitas via Ajax, usando o AngularJs.
+
+Foi usado o SASS para facilitar a criação de css, uso o ScoutApp que faz a conversão em tempo real.
+
+Algumas vezes quando a página não atualiza quando crio um nova tarefa, creio ser algo relativo a função:  $scope.$apply();
+
+A aplicação esta bem responsiva.
+
+
+
+
---
+++ SQL.sql
@@ -0,0 +1,30 @@
+-- --------------------------------------------------------
+-- Servidor:                     127.0.0.1
+-- Versão do servidor:           5.6.24 - MySQL Community Server (GPL)
+-- OS do Servidor:               Win32
+-- HeidiSQL Versão:              9.2.0.4947
+-- --------------------------------------------------------
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8mb4 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+
+-- Copiando estrutura do banco de dados para fazer_feito
+CREATE DATABASE IF NOT EXISTS `fazer_feito` /*!40100 DEFAULT CHARACTER SET utf8 */;
+USE `fazer_feito`;
+
+
+-- Copiando estrutura para tabela fazer_feito.tarefas
+CREATE TABLE IF NOT EXISTS `tarefas` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `descricao` varchar(255) DEFAULT NULL,
+  `ativa` char(50) DEFAULT 'S',
+  `prioridade` int(11) DEFAULT '3',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- Exportação de dados foi desmarcado.
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
